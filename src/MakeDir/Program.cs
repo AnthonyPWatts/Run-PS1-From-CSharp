d775307af6c3e10e5449974ccde536cebc52e79b
Anthony Watts <anthonypwatts@gmail.com>
2021-04-04 17:23:29 +0100

    Example works as expected

--- src/MakeDir/Program.cs
+++ src/MakeDir/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.IO;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
 
 namespace MakeDir
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var runSpace = RunspaceFactory.CreateRunspace();
+            try
+            {
+                var powershell = PowerShell.Create();
+                runSpace.Open();
+                powershell.Runspace = runSpace;
+
+                powershell.AddScript("Set-ExecutionPolicy Unrestricted -Scope CurrentUser ");
+                powershell.Invoke();
+
+                powershell.AddCommand(Path.Combine(Directory.GetCurrentDirectory(), "MakeDir.ps1"));
+                powershell.AddParameter("dirName", "_MyPowershellDir_");
+
+                var results = powershell.Invoke();
+                if (powershell.Streams.Error.Count > 0)
+                {
+                    Console.WriteLine("Failed");
+                    foreach (var error in powershell.Streams.Error)
+                    {
+                        Console.WriteLine(error.ErrorDetails.Message);
+                    }
+
+                    return -1;
+                }
+
+                powershell.Commands.Clear();
+                powershell.AddScript("Set-ExecutionPolicy Restricted -Scope CurrentUser");
+                powershell.Invoke();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERRORS OCCURRED! " + ex.Message);
+                return -2;
+            }
+
+
+            Console.WriteLine("Done");
+            return 200;
         }
     }
 }
